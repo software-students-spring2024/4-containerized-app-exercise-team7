011a58f151344570e2f24fb99790ef167f13a42b
denprud <denzelprud1@gmail.com>
2024-04-13 17:38:04 -0400

    Network-Interconnection

--- web_app/app.py
+++ web_app/app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from pymongo import MongoClient
 import os
 from bson.objectid import ObjectId
 import datetime
+import requests
 
 load_dotenv()
 
-app = Flask(__name__)
 
-mongo_uri = os.getenv("MONGO_URI")
-mongo_dbname = os.getenv("MONGO_DBNAME")
-client = MongoClient(mongo_uri)
-db = client[mongo_dbname]
+app = Flask(__name__)
+app.secret_key = '232323112@@11'
+
+# mongo_uri = os.getenv("MONGO_URI")
+# mongo_dbname = os.getenv("MONGO_DBNAME")
+# client = MongoClient(mongo_uri)
+# db = client[mongo_dbname]
 
 @app.route("/")
 def home():
@@ -30,5 +33,21 @@ def see():
 def notes():
     return render_template("analyze.html")
 
+@app.route("/send", methods=['GET', 'POST'])
+def send_file_to_ml_app():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            url = 'http://goofy_austin:5001/upload'
+            files = {'file': (file.filename, file)}
+            response = requests.post(url, files=files)
+            return response.json()
+    return render_template('upload.html')
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("FLASK_PORT", 5000))
+    app.run(host='0.0.0.0', debug=True, port=os.getenv("FLASK_PORT", 5000))
