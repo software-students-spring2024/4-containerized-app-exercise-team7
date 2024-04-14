37c4e7b351cd164d142c22c88d690859af9178a7
nhaulsey <ndhaulsey@gmail.com>
2024-04-14 12:06:50 -0400

    Added unit test for the home route.

--- web_app/tests/test.py
+++ web_app/tests/test.py
@@ -3,17 +3,32 @@ import pytest
 from web_app.app import *
 from web_app.app import app
 
+@pytest.fixture
 
-class Tests:
+def client():
+    
+    app.config.update({
+        
+        "TESTING" : True,
+    })
+    with app.test_client() as client:
+        yield client
 
-    def test_sanity_check(self):
 
-        expected = True
-        actual = True
-        assert actual == expected, "Expected True to be equal to True!"
+def test_sanity_check(client):
 
-    def test_sanity_check2(self):
+    expected = True
+    actual = True
+    assert actual == expected, "Expected True to be equal to True!"
 
-        expected = True
-        actual = True
-        assert actual == expected, "Expected True to be equal to True!"
+def test_sanity_check2(client):
+
+    expected = True
+    actual = True
+    assert actual == expected, "Expected True to be equal to True!"
+
+def test_home_page(client):
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Welcome to Melody Note Finder" in response.data
