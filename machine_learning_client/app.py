f4d91945280bafba6d504890abdd2044f4992919
RakSridhar23 <rss9243@nyu.edu>
2024-04-14 10:39:29 -0400

    added some features to ml

--- machine_learning_client/app.py
+++ machine_learning_client/app.py
@@ -16,9 +16,30 @@ def extract_audio_feature(audio_file):
     
     y, sr = librosa.load(audio_file)
     
-    feature = librosa.feature.mfcc(y=y, sr=sr)
+   # trimmed, index = librosa.effects.trim(y, top_db=20)
     
-    return feature.tolist()
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    
+    # Tempo estimate
+    
+    temp = librosa.beat.tempo(y=y, sr=sr)[0]
+    
+    #beat tracking 
+
+    
+    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+    beat_times = librosa.frames_to_time(beats, sr =sr)
+    
+    feature ={
+        'mfcc': mfcc.tolist(),
+        'temp': temp,
+        'tempo': tempo,
+        'beats': beats.tolist()    
+    }
+    
+    
+    
+    return feature
 
 @app.route('/upload', methods=['POST'])
 def audio_file():
@@ -34,16 +55,20 @@ def audio_file():
         return jsonify({'error': 'File not supported'}), 400
     
     if file:
-        fn = file.filename
-        audio_features = extract_audio_feature(io.BytesIO(file.read()))
+        try:
+            fn = file.filename
+            audio_features = extract_audio_feature(io.BytesIO(file.read()))
         
-        data={
-            'filename': fn,
-            'features': audio_features
-            }
-        AudioFeature.insert_one(data)
+            data={
+                'filename': fn,
+                'feature': audio_features
+                }
+            AudioFeature.insert_one(data)
         
-        return jsonify({'message': 'Upload Successful!'}), 200
+            return jsonify({'message': 'Upload Successful!'}), 200
+        
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5001)
